d5014083f5bb6908be9b6bb92c666dcf59c67586
mratet <mratet.vignat@gmail.com>
2024-11-17 15:36:10 +0100

    Add zsh plugins add some packages

--- run_once_install_ohmyzsh.sh
+++ run_once_install_ohmyzsh.sh
@@ -3,6 +3,12 @@
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo "Getting ohmyz.sh"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
+
+	# Install plugins
+	sh -c "$(git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions)"
+	sh -c "$(git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use)"
+	sh -c "$(git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting)"
 else
 	echo ".oh-my-zsh already found, skipping."
 fi
+

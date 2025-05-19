b2211d90d2411abb08f99be5eefa22a92b680d50
mratet <122007098+mratet@users.noreply.github.com>
2025-05-19 18:49:17 +0200

    Update run_once_install_ohmyzsh.sh
    Update ohmyzsh install

--- run_once_install_ohmyzsh.sh
+++ run_once_install_ohmyzsh.sh
@@ -6,8 +6,13 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 
 	# Install plugins
 	sh -c "$(git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions)"
-	sh -c "$(git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use)"
+  	sh -c "$(git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use)"
 	sh -c "$(git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting)"
+	sh -c "$(git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k)"
+
+ 	# Install env utilities
+  	sh -c "curl -fsSL https://pyenv.run | sh" 
+   	sh -c "curl -#fLo- 'https://raw.githubusercontent.com/hyperupcall/autoenv/main/scripts/install.sh' | sh"
 else
 	echo ".oh-my-zsh already found, skipping."
 fi

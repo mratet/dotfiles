29d45c37bc036117a37ac9f35ad45bcd0686b04b
mratet <122007098+mratet@users.noreply.github.com>
2025-05-19 18:53:27 +0200

    Update run_once_install_ohmyzsh.sh

--- run_once_install_ohmyzsh.sh
+++ run_once_install_ohmyzsh.sh
@@ -8,7 +8,7 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	sh -c "$(git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions)"
   	sh -c "$(git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use)"
 	sh -c "$(git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting)"
-	sh -c "$(git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k)"
+	sh -c "$(git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k)"
 
  	# Install env utilities
   	sh -c "curl -fsSL https://pyenv.run | sh" 
